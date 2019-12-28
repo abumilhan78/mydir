ee608d34e73781b84af54f712fa8007e68815c7d
abumilhan78 <abumilhan78@gmail.com>
2019-12-29 01:22:50 +0700

    Beginner Moves

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layout/main')
+
+@section('title','Web Programming Assalaam')
+
+@section('container')
+  	<div class="container">
+  		<div class="row">
+  			<div class="col-10">
+  				 <h1 class="mt-2">Hello, world!</h1>
+  			</div>
+  		</div>
+  	</div>
+@endsection
+  
